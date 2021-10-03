63333e9d13072c56ed4757df2297330eb4537ecf
Diegox777 <diego.adrian.777@gmail.com>
2021-10-02 22:20:33 -0400

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // run `node index.js` in the terminal
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = express();
+
+const PORT = 8001;
+
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
+});
